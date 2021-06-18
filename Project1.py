7b3b5a3bc3d2602417bf3c73b9699fff09617562
Alessandro <64541478+AlessandroPio@users.noreply.github.com>
2021-06-18 19:34:38 +0200

    fin 2

--- Project1.py
+++ Project1.py
@@ -3,11 +3,16 @@ from __future__ import division
 from Bio import SeqIO, Phylo
 from Bio import AlignIO
 from Bio import Seq
+from Bio.SeqUtils import ProtParam
 from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
 import os, glob
+import pandas as pd
 
-def writeToFile(tree, input_file):
-    Phylo.write(tree, "sequences/" + input_file + '_phylogeny.txt', "phyloxml")
+def writeToFileTree(tree, output_file):
+    Phylo.write(tree, "sequences/" + output_file + "_PyloTree.xml", "phyloxml")
+
+def writeToFile(content,file):
+    file.write(content + "\n")
 
 def dotLenght(input_file):
 
@@ -39,27 +44,9 @@ def phylogeneticTree(alignments):
     distanceConstructor = DistanceTreeConstructor()
     tree = distanceConstructor.nj(distanceMatrix)
     #tree.clade[0, 1].color = "blue"
-    writeToFile(tree, input_file)
+    writeToFileTree(tree, input_file)
 
 def genomeAnalysis(file):
-    for sequence in SeqIO.parse('sequences/' + file + ".fasta", "fasta"):
-        print(sequence.seq)
-        print(len(sequence), 'nucliotides')
-    dnaSequence = SeqIO.read('sequences/' + file + ".fasta", "fasta")
-    dna = dnaSequence.seq  # Convert DNA into mRNA Sequence
-    mRNA = dna.transcribe()  # Transcribe a DNA sequence into RNA.
-    print(mRNA)
-    print('Size : ', len(mRNA))
-    Amino_Acid = mRNA.translate(table=1, cds=False)
-    print('Amino Acid', Amino_Acid)
-    print("Length of Protein:", len(Amino_Acid))
-    print("Length of Original mRNA:", len(mRNA))
-
-    Proteins = Amino_Acid.split('*')  # * is translated stop codon
-    df = pd.DataFrame(Proteins)
-    df.describe()
-    print('Total proteins:', len(df))
-
     def conv(item):
         return len(item)
 
@@ -67,30 +54,67 @@ def genomeAnalysis(file):
             return str(item)
             df['sequence_str'] = df[0].apply(to_str)
 
-    poi_list = []
-    MW_list = []
-    from Bio.SeqUtils import ProtParam
-    for record in Proteins[:]:
-        print("\n")
+    def getProtein():
         X = ProtParam.ProteinAnalysis(str(record))
-        POI = X.count_amino_acids()
-        poi_list.append(POI)
-        MW = X.molecular_weight()
-        MW_list.append(MW)
-        print("Protein of Interest = ", POI)
-        print("Amino acids percent =    ", str(X.get_amino_acids_percent()))
-        print("Molecular weight = ", MW_list)
-        print("Aromaticity = ", X.aromaticity())
-        print("Flexibility = ", X.flexibility())
-        print("Isoelectric point = ", X.isoelectric_point())
-        print("Secondary structure fraction = ", X.secondary_structure_fraction())
+        protein_of_interest = X.count_amino_acids()
+        p_o_i_list.append(protein_of_interest)
+        molecular_weight = X.molecular_weight()
+        molecular_weight_list.append(molecular_weight)
 
-    df['length'] = df[0].apply(conv)
-    df.rename(columns={0: "sequence"}, inplace=True)
-    df.head()  # Take only longer than 20
-    functional_proteins = df.loc[df['length'] >= 20]
-    print('Total functional proteins:', len(functional_proteins))
-    functional_proteins.describe()
+        writeToFile("PROTEIN OF INTEREST         N° -> " + str(protein_of_interest), file_output)
+        writeToFile("AMINO ACIDS PERCENT         N° -> " + str(X.get_amino_acids_percent()), file_output)
+        writeToFile("MOLECULAR WEIGHT            N° -> " + str(X.aromaticity()), file_output)
+        writeToFile("FLEXIBILITY                 N° -> " + str(X.flexibility()), file_output);
+
+        #print("Aromaticity = ", X.aromaticity())
+        #print("Isoelectric point = ", X.isoelectric_point())
+        #print("Secondary structure fraction = ", X.secondary_structure_fraction())
+
+    file_output = open("sequences/" + input_file + "_phylogenetic_analysis.txt", "w+")
+    cont = 0
+    for sequence in SeqIO.parse('sequences/' + file + ".fasta", "fasta"):
+
+        dnaSequence = sequence.seq
+        nucleotide_of_sequence = len(sequence)
+        mRNA = dnaSequence.transcribe()  # Transcribe a DNA sequence into RNA.
+        mRna_lenght = len(mRNA)
+
+        Amino_Acid = mRNA.translate(table=1, cds=False)
+        #print('Amino Acid', Amino_Acid)
+        #print("Length of Protein:", len(Amino_Acid))
+        #print("Length of Original mRNA:", len(mRNA))
+
+        Proteins = Amino_Acid.split('*')  # * is translated stop codon
+        df = pd.DataFrame(Proteins)
+        df.describe()
+        total_protein = len(df)
+
+        cont += 1
+        writeToFile("SEQUENCE           N° -> " + str(sequence.id) + " (" + str(cont) + ")", file_output);writeToFile("-----------------------------------", file_output)
+        writeToFile("NUCLEOTIDE         N° -> " + str(nucleotide_of_sequence), file_output)
+        writeToFile("DNA                N° -> " + str(len(dnaSequence)), file_output)
+        writeToFile("mRNA               N° -> " + str(mRna_lenght), file_output)
+        writeToFile("PROTEIN            N° -> " + str(total_protein), file_output);writeToFile("-----------------------------------", file_output)
+        writeToFile("mRNA SEQUENCE         -> " + str(mRNA), file_output)
+        writeToFile("DNA  SEQUENCE         -> " + str(dnaSequence), file_output)
+        writeToFile("AMINO ACID            -> " + str(Amino_Acid), file_output);writeToFile("-----------------------------------", file_output)
+
+        p_o_i_list = []
+        molecular_weight_list = []
+
+        for record in Proteins[:]:
+            try:
+                getProtein()
+            except:
+                continue
+
+
+       # df['length'] = df[0].apply(conv)
+       # df.rename(columns={0: "sequence"}, inplace=True)
+       # df.head()  # Take only longer than 20
+       # functional_proteins = df.loc[df['length'] >= 20]
+       # print('Total functional proteins:', len(functional_proteins))
+       # functional_proteins.describe()
 
 while True:
 
@@ -117,7 +141,7 @@ while True:
             print("| " + file)
         print("|");print("----")
 
-phylogeneticTree(phylipTrascription(input_file))
-genomeAnalysis(input_file)
-
+phylogeneticTree(phylipTrascription(input_file))  # creo l'albero filogenetico
+genomeAnalysis(input_file)                        # parto con l'analisi genomica delle sequenze
+file.close()
 print("| Processo terminato!")
